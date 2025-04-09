37353d17ba2068c762ffd9320eed8764bb0adf35
chensoul <ichensoul@gmail.com>
2025-04-09 17:14:43 +0800

    fix: CachingRequestFilter

--- chensoul-spring-boot/chensoul-spring-boot-common/src/main/java/com/chensoul/spring/boot/filter/CachingRequestFilter.java
+++ chensoul-spring-boot/chensoul-spring-boot-common/src/main/java/com/chensoul/spring/boot/filter/CachingRequestFilter.java
@@ -1,5 +1,6 @@
 package com.chensoul.spring.boot.filter;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.util.StreamUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -7,59 +8,64 @@ import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletRequestWrapper;
 import javax.servlet.http.HttpServletResponse;
-import java.io.BufferedReader;
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 /**
  * @author zhijun.chen
  * @since 2.16.3
  */
+@Slf4j
 public class CachingRequestFilter extends OncePerRequestFilter {
-	@Override
-	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
-		ServletRequest requestWrapper = new ContentCachingRequestWrapper(request);
-		chain.doFilter(requestWrapper, response);
-	}
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
+        ServletRequest requestWrapper = new ContentCachingRequestWrapper(request);
+        chain.doFilter(requestWrapper, response);
+    }
 
-	public class ContentCachingRequestWrapper extends HttpServletRequestWrapper {
-		private final byte[] body;
+    public class ContentCachingRequestWrapper extends HttpServletRequestWrapper {
+        private final byte[] body;
 
-		public ContentCachingRequestWrapper(HttpServletRequest request) throws IOException {
-			super(request);
-			request.setCharacterEncoding("UTF-8");
-			body = StreamUtils.copyToByteArray(request.getInputStream());
-		}
+        public ContentCachingRequestWrapper(HttpServletRequest request) throws IOException {
+            super(request);
+            this.body = readRequestBody(request);
+        }
 
-		@Override
-		public BufferedReader getReader() throws IOException {
-			return new BufferedReader(new InputStreamReader(getInputStream()));
-		}
+        private byte[] readRequestBody(HttpServletRequest request) throws IOException {
+            request.setCharacterEncoding("UTF-8");
+            try (InputStream inputStream = request.getInputStream()) {
+                return StreamUtils.copyToByteArray(inputStream);
+            }
+        }
 
-		@Override
-		public ServletInputStream getInputStream() throws IOException {
-			final ByteArrayInputStream bais = new ByteArrayInputStream(body);
-			return new ServletInputStream() {
-				@Override
-				public int read() throws IOException {
-					return bais.read();
-				}
+        @Override
+        public BufferedReader getReader() throws IOException {
+            return new BufferedReader(new InputStreamReader(getInputStream()));
+        }
 
-				@Override
-				public boolean isFinished() {
-					return Boolean.FALSE;
-				}
+        @Override
+        public ServletInputStream getInputStream() throws IOException {
+            final ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(body);
+            return new ServletInputStream() {
+                @Override
+                public int read() throws IOException {
+                    return byteArrayInputStream.read();
+                }
 
-				@Override
-				public boolean isReady() {
-					return Boolean.FALSE;
-				}
+                @Override
+                public boolean isFinished() {
+                    return byteArrayInputStream.available() == 0;
+                }
 
-				@Override
-				public void setReadListener(ReadListener readListener) {
-				}
-			};
-		}
-	}
+                @Override
+                public boolean isReady() {
+                    return true;
+                }
+
+                @Override
+                public void setReadListener(ReadListener readListener) {
+                    throw new UnsupportedOperationException("ReadListener is not supported");
+                }
+            };
+        }
+    }
 }
