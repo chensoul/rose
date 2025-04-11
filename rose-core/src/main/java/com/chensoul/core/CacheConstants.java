ef6987693268edf8865c550c8199631621d75bba
chensoul <ichensoul@gmail.com>
2025-04-11 08:24:16 +0800

    build: rose

--- rose-core/src/main/java/com/chensoul/core/CacheConstants.java
+++ rose-core/src/main/java/com/chensoul/core/CacheConstants.java
@@ -7,41 +7,41 @@ package com.chensoul.core;
  */
 public interface CacheConstants {
 
-	/**
-	 * 菜单信息缓存的key
-	 */
-	String MENU_DETAIL = "rose:menu_detail";
+    /**
+     * 菜单信息缓存的key
+     */
+    String MENU_DETAIL = "rose:menu_detail";
 
-	/**
-	 * 用户信息缓存的key
-	 */
-	String USER_DETAIL = "rose:user_detail";
+    /**
+     * 用户信息缓存的key
+     */
+    String USER_DETAIL = "rose:user_detail";
 
-	/**
-	 * 字典信息缓存的key
-	 */
-	String DICT_DETAIL = "rose:dict_detail";
+    /**
+     * 字典信息缓存的key
+     */
+    String DICT_DETAIL = "rose:dict_detail";
 
-	/**
-	 * oauth 客户端缓存的key，值为hash
-	 */
-	String OAUTH_CLIENT_DETAIL = "rose:oauth_client_detail";
+    /**
+     * oauth 客户端缓存的key，值为hash
+     */
+    String OAUTH_CLIENT_DETAIL = "rose:oauth_client_detail";
 
-	/**
-	 * tokens缓存的key
-	 */
-	String OAUTH_TOKENS = "rose:oauth_tokens";
+    /**
+     * tokens缓存的key
+     */
+    String OAUTH_TOKENS = "rose:oauth_tokens";
 
-	/**
-	 * oauth token store 存储前缀
-	 */
-	String OAUTH_TOKEN_STORE_PREFIX = "rose:oauth_token_store:";
+    /**
+     * oauth token store 存储前缀
+     */
+    String OAUTH_TOKEN_STORE_PREFIX = "rose:oauth_token_store:";
 
-	String SHOP = "rose:shop";
+    String SHOP = "rose:shop";
 
-	String TENANT_APP_CONFIG = "rose:tenant_app_config";
+    String TENANT_APP_CONFIG = "rose:tenant_app_config";
 
-	String CLIENT = "rose:client";
+    String CLIENT = "rose:client";
 
-	String LOGIN_FAIL_COUNT_CACHE = "rose:loginFailCount:";
+    String LOGIN_FAIL_COUNT_CACHE = "rose:loginFailCount:";
 }
--- rose-security/src/main/java/com/chensoul/security/SecurityProperties.java
+++ rose-security/src/main/java/com/chensoul/security/SecurityProperties.java
@@ -1,14 +1,13 @@
 package com.chensoul.security;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.util.CollectionUtils;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -16,50 +15,50 @@ import java.util.List;
 @ConfigurationProperties(prefix = "security", ignoreUnknownFields = false)
 public class SecurityProperties {
 
-	public static final String FORM_BASED_LOGIN_ENTRY_POINT = "/api/auth/login";
+    public static final String FORM_BASED_LOGIN_ENTRY_POINT = "/api/auth/login";
 
-	public static final String TOKEN_REFRESH_ENTRY_POINT = "/api/auth/token";
+    public static final String TOKEN_REFRESH_ENTRY_POINT = "/api/auth/token";
 
-	public static final String TOKEN_BASED_AUTH_ENTRY_POINT = "/api/**";
+    public static final String TOKEN_BASED_AUTH_ENTRY_POINT = "/api/**";
 
-	public static final String[] DEFAULT_PATH_TO_SKIP = new String[]{
-		TOKEN_REFRESH_ENTRY_POINT,
-		FORM_BASED_LOGIN_ENTRY_POINT,
-		"/api/noauth/**",
-		"/error",
-		"/actuator/**",
-		"/api/upms/mail/oauth2/code"
-	};
+    public static final String[] DEFAULT_PATH_TO_SKIP = new String[] {
+        TOKEN_REFRESH_ENTRY_POINT,
+        FORM_BASED_LOGIN_ENTRY_POINT,
+        "/api/noauth/**",
+        "/error",
+        "/actuator/**",
+        "/api/upms/mail/oauth2/code"
+    };
 
-	private String baseUrl = TOKEN_BASED_AUTH_ENTRY_POINT;
+    private String baseUrl = TOKEN_BASED_AUTH_ENTRY_POINT;
 
-	private String loginUrl = FORM_BASED_LOGIN_ENTRY_POINT;
+    private String loginUrl = FORM_BASED_LOGIN_ENTRY_POINT;
 
-	private String tokenRefreshUrl = TOKEN_REFRESH_ENTRY_POINT;
+    private String tokenRefreshUrl = TOKEN_REFRESH_ENTRY_POINT;
 
-	private List<String> pathsToSkip = new ArrayList<>();
+    private List<String> pathsToSkip = new ArrayList<>();
 
-	private Long accessTokenExpireTime = 9000L;
+    private Long accessTokenExpireTime = 9000L;
 
-	private Long refreshTokenExpireTime = 604800L;
+    private Long refreshTokenExpireTime = 604800L;
 
-	private JwtProperties jwt = new JwtProperties();
+    private JwtProperties jwt = new JwtProperties();
 
-	public List<String> getPathsToSkip() {
-		if (CollectionUtils.isEmpty(pathsToSkip)) {
-			pathsToSkip.addAll(Arrays.asList(DEFAULT_PATH_TO_SKIP));
-		}
-		return pathsToSkip;
-	}
+    public List<String> getPathsToSkip() {
+        if (CollectionUtils.isEmpty(pathsToSkip)) {
+            pathsToSkip.addAll(Arrays.asList(DEFAULT_PATH_TO_SKIP));
+        }
+        return pathsToSkip;
+    }
 
-	@Data
-	public static class JwtProperties {
+    @Data
+    public static class JwtProperties {
 
-		private boolean enabled = false;
+        private boolean enabled = false;
 
-		private String tokenIssuer = "rose.chensoul.com";
+        private String tokenIssuer = "rose.chensoul.com";
 
-		private String tokenSigningKey =
-			"secret12345678901234567890123456789012345678901234567890123456789012345678901234567890";
-	}
+        private String tokenSigningKey =
+                "secret12345678901234567890123456789012345678901234567890123456789012345678901234567890";
+    }
 }
