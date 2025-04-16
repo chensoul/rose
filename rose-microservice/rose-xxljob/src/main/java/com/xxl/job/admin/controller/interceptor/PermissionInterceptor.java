0a3fe39ff8fc1f25e03ba449116dcb84bb39f069
chensoul <ichensoul@gmail.com>
2025-04-16 16:28:05 +0800

    修改github action

--- rose-microservice/rose-xxljob/src/main/java/com/xxl/job/admin/controller/interceptor/PermissionInterceptor.java
+++ rose-microservice/rose-xxljob/src/main/java/com/xxl/job/admin/controller/interceptor/PermissionInterceptor.java
@@ -26,45 +26,6 @@ public class PermissionInterceptor implements AsyncHandlerInterceptor {
     @Resource
     private LoginService loginService;
 
-    @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
-            throws Exception {
-
-        if (!(handler instanceof HandlerMethod)) {
-            return true; // proceed with the next interceptor
-        }
-
-        // if need login
-        boolean needLogin = true;
-        boolean needAdminuser = false;
-        HandlerMethod method = (HandlerMethod) handler;
-        PermissionLimit permission = method.getMethodAnnotation(PermissionLimit.class);
-        if (permission != null) {
-            needLogin = permission.limit();
-            needAdminuser = permission.adminuser();
-        }
-
-        if (needLogin) {
-            XxlJobUser loginUser = loginService.ifLogin(request, response);
-            if (loginUser == null) {
-                response.setStatus(302);
-                response.setHeader("location", request.getContextPath() + "/toLogin");
-                return false;
-            }
-            if (needAdminuser && loginUser.getRole() != 1) {
-                throw new RuntimeException(I18nUtil.getString("system_permission_limit"));
-            }
-            request.setAttribute(LoginService.LOGIN_IDENTITY_KEY, loginUser); // set
-            // loginUser,
-            // with
-            // request
-        }
-
-        return true; // proceed with the next interceptor
-    }
-
-    // -------------------- permission tool --------------------
-
     /**
      * get loginUser
      *
@@ -78,6 +39,8 @@ public class PermissionInterceptor implements AsyncHandlerInterceptor {
         // request
         return loginUser;
     }
+
+    // -------------------- permission tool --------------------
 
     /**
      * valid permission by JobGroup
@@ -121,5 +84,42 @@ public class PermissionInterceptor implements AsyncHandlerInterceptor {
             }
         }
         return jobGroupList;
+    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+            throws Exception {
+
+        if (!(handler instanceof HandlerMethod)) {
+            return true; // proceed with the next interceptor
+        }
+
+        // if need login
+        boolean needLogin = true;
+        boolean needAdminuser = false;
+        HandlerMethod method = (HandlerMethod) handler;
+        PermissionLimit permission = method.getMethodAnnotation(PermissionLimit.class);
+        if (permission != null) {
+            needLogin = permission.limit();
+            needAdminuser = permission.adminuser();
+        }
+
+        if (needLogin) {
+            XxlJobUser loginUser = loginService.ifLogin(request, response);
+            if (loginUser == null) {
+                response.setStatus(302);
+                response.setHeader("location", request.getContextPath() + "/toLogin");
+                return false;
+            }
+            if (needAdminuser && loginUser.getRole() != 1) {
+                throw new RuntimeException(I18nUtil.getString("system_permission_limit"));
+            }
+            request.setAttribute(LoginService.LOGIN_IDENTITY_KEY, loginUser); // set
+            // loginUser,
+            // with
+            // request
+        }
+
+        return true; // proceed with the next interceptor
     }
 }
