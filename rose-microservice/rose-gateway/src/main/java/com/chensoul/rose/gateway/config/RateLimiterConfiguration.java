9dd81ea880e22f3a689caf200af277d2861e33e3
chensoul <ichensoul@gmail.com>
2025-04-17 16:47:38 +0800

    add maven plugins

--- rose-microservice/rose-gateway/src/main/java/com/chensoul/rose/gateway/config/RateLimiterConfiguration.java
+++ rose-microservice/rose-gateway/src/main/java/com/chensoul/rose/gateway/config/RateLimiterConfiguration.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020 pig4cloud Authors. All Rights Reserved.
+ * Copyright © 2025 Chensoul, Inc. (ichensoul@gmail.com)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.chensoul.rose.gateway.config;
 
 import java.util.Objects;
--- rose-spring-boot/rose-spring-boot-oss/src/main/java/com/chensoul/rose/oss/old/storage/cloud/qiniu/QiNiuScope.java
+++ rose-spring-boot/rose-spring-boot-oss/src/main/java/com/chensoul/rose/oss/old/storage/cloud/qiniu/QiNiuScope.java
@@ -1,12 +1,9 @@
 /*
- * Copyright (c) 2023 WEMIRR-PLATFORM Authors. All Rights Reserved.
+ * Copyright © 2025 Chensoul, Inc. (ichensoul@gmail.com)
  *
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
@@ -16,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.chensoul.rose.oss.old.storage.cloud.qiniu;
 
 /**
